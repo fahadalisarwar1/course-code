9f490eeeaf2e93eb17ab091d3768cbe13412458b
Fahad Ali Sarwar <32795677+fahadalisarwar1@users.noreply.github.com>
2020-12-31 00:02:13 +0100

    added example 4 blink

---
+++ Chapter1-Introduction/example4-blink-LED/main/main.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+#include "driver/gpio.h" // this will help us to access the GPIO.
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+/*
+    In this example we are going to blink an led,
+    you can either select the internal LED or attach an external one. 
+     
+*/
+#define LED_PIN 2
+void app_main(void)
+{
+    // GPIO can be either ADC, DAC, touch, etc. Here we enable the GPIO mode. 
+    gpio_pad_select_gpio(LED_PIN); // This sets the pin as GPIO, note that pins are multiplexed and they have different modees. 
+
+
+    // Next we will set the direction of GPIO, GPIO can be either input or output, 
+    // We want the LED to blink so it will be output in our case.
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);   
+
+    // Now we simply toggle the pin after a delay
+    int state = 1; // ON state
+    while (1)
+    {
+
+        gpio_set_level(LED_PIN, state); // simply passing the state level to LED_PIN
+        printf("LED is %d\n", state);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+        
+        state = !state; // Toggle the LED
+        /* code */
+    }
+    
+    
+}
