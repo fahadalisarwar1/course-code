7f337c7ae0286c4a4199680d411c632941f4b11a
Fahad Ali Sarwar <32795677+fahadalisarwar1@users.noreply.github.com>
2021-01-06 13:52:40 +0100

    added example basic c

--- Chapter3-C_tutorial/example1-basics/main/main.c
+++ Chapter3-C_tutorial/example1-basics/main/main.c
@@ -1,21 +1,45 @@
 
 #include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include <string.h>
 
 
+struct  Person
+{
+    // data regarding the struct should be placed here
+    char firstName[20];
+    char lastName[20];
+    int age;      
+};
+
+typedef struct Person_struct{
+    int age;
+    char school[20];
+    char firstName[20];
+}Person;
+    
 void app_main(void)
 {
 
-    printf("[+] This is a initialization\n");
-    int i = 0;
-    while (1)
-        {
-        printf("[+] This is a loop number %d\n", i++);
-        vTaskDelay(1000/ portTICK_PERIOD_MS);
-        
-        /* code */
-    }
-    
+    /*
+    In this example we will learn more about C programming
 
+    */
+   struct Person p; // here we have to write the struct keyword as no alias is used for the Person
+
+   strcpy(p.firstName, "fahad");
+   strcpy(p.lastName, "ali");
+   p.age=12;
+
+   printf("%s %s is %d years old\n", p.firstName, p.lastName, p.age);
+   
+    Person p2;
+    strcpy(p2.firstName, "fahad");
+    strcpy(p2.school, "fauji");
+    p2.age =12;
+
+    printf("%s studied in  %s school for %d years\n", p2.firstName, p2.school, p2.age);
+
+
+   
+   
 }
